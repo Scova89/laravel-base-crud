ae214628360dd125b3cd3c1f76f79bf1c0108c56
Francesco <scovascova@gmail.com>
2022-02-11 16:51:53 +0100

    tabella dei valori in index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,11 +1,27 @@
 @extends('layouts.base')
 
 @section('pageContent')
-    <ul>
-        @foreach ($comics as $comic)
-        <li>{{$comic->title}}</li>
-            
-        @endforeach
-    </ul>
+    
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Copertina</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Prezzo</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($comics as $comic)
+            <tr>
+                <td>{{$comic->id}}</td>
+                <td><img src="{{$comic->thumb}}" alt="{{$comic->title}}"></td>
+                <td>{{$comic->title}}</td>
+                <td>{{$comic->price}}</td>
+            </tr>
+          @endforeach
+          
+        </tbody>
+      </table>
     
 @endsection
