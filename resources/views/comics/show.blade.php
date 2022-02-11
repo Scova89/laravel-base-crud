6e72b8e23e05d742066ffac209db264eb50cdd3f
Francesco <scovascova@gmail.com>
2022-02-11 22:13:24 +0100

    route per pulsante indietro

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,5 +4,6 @@
     <h1>{{$comic->title}}</h1>
     <img src="{{$comic->thumb}}" alt="{{$comic->title}}">
     <p>{{$comic->description}}</p>
+    <a href="{{route("comics.index")}}"><button type="button" class="btn btn-secondary">Indietro</button></a>
     
 @endsection
