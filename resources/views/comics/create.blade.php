a4ab90f6c4df1bcff84cfa699642c55d0a7d72a6
Francesco <scovascova@gmail.com>
2022-02-14 16:41:28 +0100

    messaggi di errore bootstrap

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -48,7 +48,7 @@
     </form>
 
     @if ($errors->any())
-        <div class="alert alert-danger">
+        <div class="alert alert-danger mt-4">
             <ul>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
